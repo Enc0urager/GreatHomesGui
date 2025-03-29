ed722f6e1666bb75ffe723c93435b5699ffb750d
Encourager <df12376@yandex.ru>
2025-03-29 21:35:25 +0300

    upd

--- src/main/java/dev/enco/greatessentialsgui/actions/ActionExecutor.java
+++ src/main/java/dev/enco/greatessentialsgui/actions/ActionExecutor.java
@@ -1,7 +1,9 @@
 package dev.enco.greatessentialsgui.actions;
 
+import com.earth2me.essentials.commands.WarpNotFoundException;
 import dev.triumphteam.gui.guis.PaginatedGui;
 import lombok.experimental.UtilityClass;
+import net.ess3.api.InvalidWorldException;
 import org.bukkit.entity.Player;
 import java.util.List;
 import java.util.Map;
@@ -11,10 +13,16 @@ public class ActionExecutor {
     public void execute(Player player, PaginatedGui gui, Map<ActionType, List<String>> actions, String name) {
         actions.keySet().forEach(type -> {
             var contexts = actions.get(type);
-            contexts.forEach(context -> {
-                type.getAction().execute(player, gui, context
-                        .replace("{name}", name).replace("{player}", player.getName()));
-            });
+            for (String context : contexts) {
+                try {
+                    type.getAction().execute(player, gui, context
+                            .replace("{name}", name).replace("{player}", player.getName()));
+                } catch (WarpNotFoundException e) {
+                    throw new RuntimeException(e);
+                } catch (InvalidWorldException e) {
+                    throw new RuntimeException(e);
+                }
+            }
         });
     }
 }
